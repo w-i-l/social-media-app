b970a586a47119d78639c5e664dfb6ee656545aa
Ocnaru Mihai <masterpaper01@gmail.com>
2023-05-02 18:17:38 +0300

    functional sign in

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
+const { localsName } = require("ejs");
 const express = require("express")
 const app = express()
+
+const users = []
+let loggedIn = false
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req, res) =>{
-    res.render('SignIn/signin')
+    if(loggedIn){
+        res.render('Main/main')
+    }
+    else{
+        res.render('SignIn/signin')
+    }
 })
 
 app.get('/auth', (req, res) =>{
     res.render('Auth/auth')
+})
+
+app.post('/auth', (req, res) => {
+    users.push(req.body)
+    res.render('Main/main')
+    loggedIn = true
 })
 
 app.listen(3000)
