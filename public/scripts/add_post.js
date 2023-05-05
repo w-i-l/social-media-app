cae7606dc312ced633ffdd4ba2c7c2cfef071e11
w-i-l <masterpaper01@gmail.com>
2023-05-05 22:42:42 +0300

    styling

--- public/scripts/add_post.js
+++ public/scripts/add_post.js
@@ -3,7 +3,7 @@ window.onload = () => {
     // const username_form = document.getElementsByName('username')[0];
     const description_form = document.getElementsByName('description')[0];
     const image_form = document.getElementsByName('image')[0];
-    // console.log(image_form)
+    const image_label = document.getElementsByName('image_label')[0];
 
     // const post_username = document.getElementsByClassName('user')[0].getElementsByTagName('h5')[0];
     const post_description = document.getElementsByClassName('description')[0].getElementsByTagName('p')[0];
@@ -19,10 +19,22 @@ window.onload = () => {
 
     image_form.addEventListener('change', () => {
         const file = image_form.files[0];
+
+
+        if(file == undefined){
+            post_image.src = 'image.png';
+            image_label.innerText = 'Choose a file';
+            return;
+        }
+
         const reader = new FileReader();
         reader.onload = (event) => {
             post_image.src = event.target.result;
         }
         reader.readAsDataURL(file);
+        
+        const image_name = image_form.value.split('\\').at(-1);
+        image_label.innerText = image_name;
+
     })
 }
