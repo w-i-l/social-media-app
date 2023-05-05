12ac1a60fbd6bee239db4f0de8cfdb7adda5439f
w-i-l <masterpaper01@gmail.com>
2023-05-05 16:17:16 +0300

    change photo name also

--- public/scripts/add_post.js
+++ public/scripts/add_post.js
@@ -1,17 +1,17 @@
 window.onload = () => {
     
-    const username_form = document.getElementsByName('username')[0];
+    // const username_form = document.getElementsByName('username')[0];
     const description_form = document.getElementsByName('description')[0];
     const image_form = document.getElementsByName('image')[0];
     console.log(image_form)
 
-    const post_username = document.getElementsByClassName('user')[0].getElementsByTagName('h5')[0];
+    // const post_username = document.getElementsByClassName('user')[0].getElementsByTagName('h5')[0];
     const post_description = document.getElementsByClassName('description')[0].getElementsByTagName('p')[0];
     const post_image = document.getElementsByClassName('post')[0].getElementsByTagName('img')[0];
     
-    username_form.addEventListener('input', () => {
-         post_username.innerText = username_form.value;
-    })
+    // username_form.addEventListener('input', () => {
+    //      post_username.innerText = username_form.value;
+    // })
 
     description_form.addEventListener('input', () =>{
         post_description.innerText = description_form.value;
--- views/Edit_account/edit_account.cjs
+++ views/Edit_account/edit_account.cjs
@@ -2,23 +2,36 @@ const fs = require('fs');
 const path = require('path');
 const { v4 } = require('uuid');
 
-const {getUsers} = require('../Auth/auth.cjs');
+const {getUsers, getUsersPath} = require('../Auth/auth.cjs');
 const { getUser } = require('../Account/account.cjs');
-const {getUsersPath} = require('../Auth/auth.cjs')
+const {getPosts, getPostsFilePath} = require('../Main/main.cjs');
+const { json } = require('express');
 
 async function modifyUser(user) {
+
     const users = await getUsers();
   
+    let oldUsername = await getUser(user['email'])['username'];
+
     for (let i = 0; i < users.length; i++) {
-        console.log(users[i]['username'], user['username'], users[i]['email']==user['email'])
-      if (users[i]['email'] == user['email']) {
-        users[i]['username'] = user['username'];
-        break;
-      }
+        if (users[i]['email'] == user['email']) {
+            oldUsername = users[i]['username'];
+            users[i]['username'] = user['username'];
+            break;
+        }
     }
     
-    fs.writeFile(getUsersPath(), JSON.stringify(users), () => {});
+    const posts = await getPosts();
 
+    for(let i=0; i<posts.length; i++){
+        console.log(posts[i]['username'], oldUsername, posts[i]['username'] == oldUsername)
+        if(posts[i]['username'] == oldUsername){
+            posts[i]['username'] = user['username'];
+        }
+    }
+
+    fs.writeFile(getPostsFilePath(), JSON.stringify(posts), () => {});
+    fs.writeFile(getUsersPath(), JSON.stringify(users), () => {});
   }
   
 
