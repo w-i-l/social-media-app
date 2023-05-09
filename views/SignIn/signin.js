a16595a8c24f828165ac9b546a0d40ef720ab261
Ocnaru Mihai <masterpaper01@gmail.com>
2023-05-09 18:36:48 +0300

    last test vercel

--- views/SignIn/signin.js
+++ views/SignIn/signin.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
         res.redirect('/main');
     }
     else{
-        res.render('../views/SignIn/signin.ejs');
+        res.render('SignIn/signin.ejs');
     }
 })
 
