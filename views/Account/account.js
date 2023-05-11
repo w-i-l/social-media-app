434c7ebf8f7d680dd4c577dee1a282a390658410
Ocnaru Mihai <masterpaper01@gmail.com>
2023-05-11 15:57:54 +0300

    logout button

--- views/Account/account.js
+++ views/Account/account.js
@@ -10,17 +10,28 @@ const {getPostsFromUserByID, getPostsWithUsernameFrom} = require('../functions/p
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
 
+
+router.get("/", async (req, res) => {
+    
     const {cookies} = req;
     const id = cookies['id'];
     const user = await getUserByID(id);
     const username = user['username'];
-
+    
     const userPosts = await getPostsFromUserByID(id);
     const ids = userPosts.map((post) => post['username']);
     const usernamePosts = await getPostsWithUsernameFrom(userPosts)
     res.render(path.join(__dirname, 'account.ejs'), {'user':user, 'userPosts':usernamePosts.reverse(), ids:ids});
+})
+
+router.get('/logout',(req, res) => {
+    
+    console.log('f')
+    // const {cookies} = req;
+    res.cookie('id', '', {expires: new Date(0)});
+    res.clearCookie("id");
+    res.redirect('/sign');
 })
 
 router.post('/', (req, res) => {
@@ -30,5 +41,6 @@ router.post('/', (req, res) => {
     res.clearCookie("id");
     res.redirect('/sign')
 })
+
 
 module.exports = router;
